521bb0e44b9cb166746208565a3bdb2c8c59e2fa
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2025-08-19 19:42:20 +0200

    Add LightDM configuration and slick-greeter setup; remove themes script

--- awesome-wm/setup.sh
+++ awesome-wm/setup.sh
@@ -8,17 +8,21 @@ sudo apt upgrade -y
 
 # Install packges for desktop setup
 sudo apt install make xorg lightdm awesome -y
-# sudo apt install --no-install-recommends sddm -y
 
+sudo cp -f ${builddir}/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp -f ${builddir}/slick-greeter.conf /etc/lightdm/slick-greeter.conf
 # Config Sddm to run on next login
 sudo dpkg-reconfigure lightdm
 
 # Setup x11 TearFree
 # if [ -d "/etc/X11/xorg.conf.d" ]; then
-#    sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/
+#    sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/20-amd.conf
 # fi
 
 # Setup Config files
 git clone https://github.com/tobi9090/dotfiles.git /home/$username/dotfiles
 git clone https://github.com/tobi9090/fonts.git /home/$username/.local/share/fonts
 bash /home/$username/dotfiles/update.sh --all
+
+sudo cp -f /home/$username/dotfiles/Wallpapers/after_sunset_landscape_2_by_rmradev_dfenf9g.jpg /usr/share/wallpapers/login.jpg
+
--- awesome-wm/themes.sh
+++
@@ -1,2 +0,0 @@
-wget -O lightdm-webkit2-greeter.deb https://software.opensuse.org/download.html?project=home:antergos&package=lightdm-webkit2-greeter
-sudo dpkg --install lightdm-webkit2-greeter.deb
