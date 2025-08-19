c1bee4ffc84279b5bf352ad17c8d1347fb04d808
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2025-08-19 14:10:53 +0200

    Refactor setup script to separate SDDM installation and update comments

--- awesome-wm/setup.sh
+++ awesome-wm/setup.sh
@@ -7,13 +7,14 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install packges for desktop setup
-sudo apt install make xorg libxcb-xfixes0-dev sddm awesome -y
+sudo apt install make xorg libxcb-xfixes0-dev awesome -y
+sudo apt install --no-install-recommends sddm -y
 
 ## Added sddm themes
 # sudo apt install qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-controls2 libqt6svg6
 sudo cp -r ${builddir}/catppuccin-macchiato /usr/share/sddm/themes/
 # Added theme to sddm.conf
-sudo cp -f ${builddir}/sddm.conf /etc/sddm.conf
+# sudo cp -f ${builddir}/sddm.conf /etc/sddm.conf
 
 # Config Sddm to run on next login
 sudo dpkg-reconfigure sddm
