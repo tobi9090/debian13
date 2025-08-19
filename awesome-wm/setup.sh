43c94c9c17ba8b61729e82ebefe1e1d335a129aa
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2025-08-19 14:38:19 +0200

    Refactor setup script to streamline SDDM installation and update theme path

--- awesome-wm/setup.sh
+++ awesome-wm/setup.sh
@@ -7,12 +7,12 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install packges for desktop setup
-sudo apt install make xorg libxcb-xfixes0-dev awesome -y
-sudo apt install --no-install-recommends sddm -y
+sudo apt install make xorg libxcb-xfixes0-dev sddm awesome -y
+# sudo apt install --no-install-recommends sddm -y
 
 ## Added sddm themes
 # sudo apt install qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-controls2 libqt6svg6
-sudo cp -r ${builddir}/catppuccin-macchiato /usr/share/sddm/themes/
+# sudo cp -r ${builddir}/catppuccin-macchiato /usr/share/sddm/themes/catppuccin-macchiato
 # Added theme to sddm.conf
 # sudo cp -f ${builddir}/sddm.conf /etc/sddm.conf
 
@@ -20,12 +20,12 @@ sudo cp -r ${builddir}/catppuccin-macchiato /usr/share/sddm/themes/
 sudo dpkg-reconfigure sddm
 
 # Setup Awesome WM and x11 TearFree
-mkdir -p "/home/$username/.config/awesome"
-sudo mkdir /usr/share/xsessions
-sudo cp -f ${builddir}/awesome.desktop /usr/share/xsessions/
-if [ -d "/etc/X11/xorg.conf.d" ]; then
-   sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/
-fi
+# mkdir -p "/home/$username/.config/awesome"
+# sudo mkdir /usr/share/xsessions
+# sudo cp -f ${builddir}/awesome.desktop /usr/share/xsessions/
+# if [ -d "/etc/X11/xorg.conf.d" ]; then
+#    sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/
+# fi
 
 # Setup Config files
 git clone https://github.com/tobi9090/dotfiles.git /home/$username/dotfiles
