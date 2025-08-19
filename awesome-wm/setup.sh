f531f083ecfeffa0ec7947c800e8cee137b8f36c
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2025-08-19 17:35:53 +0200

    Remove Catppuccin Macchiato theme files and switch from SDDM to LightDM in setup script

--- awesome-wm/setup.sh
+++ awesome-wm/setup.sh
@@ -7,22 +7,13 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install packges for desktop setup
-sudo apt install make xorg libxcb-xfixes0-dev sddm awesome -y
+sudo apt install make xorg lightdm awesome -y
 # sudo apt install --no-install-recommends sddm -y
 
-## Added sddm themes
-# sudo apt install qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-controls2 libqt6svg6
-# sudo cp -r ${builddir}/catppuccin-macchiato /usr/share/sddm/themes/catppuccin-macchiato
-# Added theme to sddm.conf
-# sudo cp -f ${builddir}/sddm.conf /etc/sddm.conf
-
 # Config Sddm to run on next login
-sudo dpkg-reconfigure sddm
+sudo dpkg-reconfigure lightdm
 
-# Setup Awesome WM and x11 TearFree
-# mkdir -p "/home/$username/.config/awesome"
-# sudo mkdir /usr/share/xsessions
-# sudo cp -f ${builddir}/awesome.desktop /usr/share/xsessions/
+# Setup x11 TearFree
 if [ -d "/etc/X11/xorg.conf.d" ]; then
    sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/
 fi
