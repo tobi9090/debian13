8fdf147e32f46bcc46562224e7d6c8280a8f8bd3
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2025-08-19 18:33:35 +0200

    Add LightDM theme installation script

---
+++ awesome-wm/themes.sh
@@ -0,0 +1,2 @@
+wget -O lightdm-webkit2-greeter.deb https://software.opensuse.org/download.html?project=home:antergos&package=lightdm-webkit2-greeter
+sudo dpkg --install lightdm-webkit2-greeter.deb
