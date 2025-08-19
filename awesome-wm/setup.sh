388ebd633387c14870bcf04682893312e7303b7c
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2025-08-19 14:59:19 +0200

    Fix package installation order and uncomment xorg configuration in setup script

--- awesome-wm/setup.sh
+++ awesome-wm/setup.sh
@@ -7,8 +7,8 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install packges for desktop setup
-sudo apt install make xorg libxcb-xfixes0-dev sddm awesome -y
-# sudo apt install --no-install-recommends sddm -y
+sudo apt install make xorg libxcb-xfixes0-dev libpam-systemd awesome -y
+sudo apt install --no-install-recommends sddm -y
 
 ## Added sddm themes
 # sudo apt install qml-module-qtquick-window2 qml-module-qtquick-layouts qml-module-qtquick-controls2 libqt6svg6
@@ -23,9 +23,9 @@ sudo dpkg-reconfigure sddm
 # mkdir -p "/home/$username/.config/awesome"
 # sudo mkdir /usr/share/xsessions
 # sudo cp -f ${builddir}/awesome.desktop /usr/share/xsessions/
-# if [ -d "/etc/X11/xorg.conf.d" ]; then
-#    sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/
-# fi
+if [ -d "/etc/X11/xorg.conf.d" ]; then
+   sudo cp -f ${builddir}/20-amd.conf /etc/X11/xorg.conf.d/
+fi
 
 # Setup Config files
 git clone https://github.com/tobi9090/dotfiles.git /home/$username/dotfiles
